e3b5698b11728b59013596c7b5c31fecc24fabf7
lorirothermel <lori@rothermel1.com>
2024-10-02 15:52:22 -0400

    Initial Commit

---
+++ IOS18Tabs/IOS18TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS18TabsApp.swift
+//  IOS18Tabs
+//
+//  Created by Lori Rothermel on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS18TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
